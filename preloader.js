8ca38f1ff35d18e675e6ce484c560fd63743b27c
Emil Norén <eminor992@gmail.com>
2018-04-22 07:52:45 +0200

    fix

---
+++ preloader.js
@@ -0,0 +1,82 @@
+/*eslint-env es6, browser*/
+
+window.itemData = new Map();
+window.slideShowGridItems = [];
+
+function returnNewEmptyGridItem() {
+    let item = document.createElement('div');
+    item.setAttribute("class", "grid-item");
+    item.innerHTML =
+        "<div class='image-ui-container'>\
+            <div class='image-bar'>\
+                <span class='image-menubutton'><div></div><div></div><div></div></span>\
+                <span class='image-title'></span>\
+            </div>\
+            <div class='image-frame'></div>\
+        </div>\
+        <div class='sixteenbynine'>\
+        </div>";
+    return item;
+}
+
+function createGridItem(folder) {
+    let gridItem = returnNewEmptyGridItem();
+    folder.gridItem = gridItem;
+    folder.title = folder.name.split(".")[0];
+    folder.thumbnailIndex = 0;
+    gridItem.children[0].children[0].children[1].innerHTML = folder.title;
+    gridItem.data = folder;
+
+
+    let thumbElements = new DocumentFragment();
+    for (let i=0; i<folder.images.length; i++) {
+        let img = document.createElement('img');
+        img.setAttribute("class", "gallery-image");
+        img.setAttribute("alt", folder.title);
+        img.setAttribute("src", folder.images[i].thumbnail);
+        if (thumbElements.childElementCount === 0) {
+            img.style.opacity = 1;
+        }
+        thumbElements.appendChild(img);
+        let r = folder.images[i].dominantColor.r;
+        let g = folder.images[i].dominantColor.g;
+        let b = folder.images[i].dominantColor.b;
+        let multiplier = 32/((r+g+b)/3)
+        folder.images[i].dominantColorDark = {
+            r: r * multiplier,
+            g: g * multiplier,
+            b: b * multiplier
+        }
+    }
+
+    gridItem.children[1].appendChild(thumbElements);
+    gridItem.children[0].children[0].style.backgroundColor = folder.images[0].dominantColorDark;
+    gridItem.children[0].children[1].style.backgroundColor = folder.images[0].dominantColorDark;
+
+    if (folder.images.length > 1) {
+        window.slideShowGridItems.push(folder);
+    }
+}
+
+function storeData(data) {
+    for (let folder of data) {
+        if (!window.itemData.has(folder.name)) {
+            createGridItem(folder);
+            window.itemData.set(folder.name, folder);
+        }
+    }
+}
+
+function startPreload() {
+    window.preload = new Promise(function(resolve) {
+        let request = new XMLHttpRequest();
+        request.onload = (function(data) {
+            let json = JSON.parse(data.target.response);
+            storeData(json.folders);
+            resolve("loaded");
+        });
+        request.open("get", "metadata.json", true);
+        request.send();
+    })
+}
+startPreload();
